e7198f2a4de52becb244e340cea1b4f4ef711d87
Junhao Zhang <junhaozhang@Junhaos-MBP.wireless.newschool.edu>
2017-02-23 11:43:24 -0500

    add cc

---
+++ creative-computering/week1/homepage.js
@@ -0,0 +1,25 @@
+// var myDate= new Date();
+
+// function checkTime(){
+// 	var currentDate= new Date();
+// 	var currentMinutes=currentDate.getMinutes();
+
+
+
+
+
+// 	if (currentMinute==58){
+// 		body.style.backgroundColor="white";
+// 		document.getElementById("image1").style.display="block";
+// 		// document.getElementById("img1").style
+// 	}
+
+// 	if (currentMinute==10){
+// 		body.style.backgroundColor="white";
+// 		document.getElementById("image2").style.display="block";
+// 		// document.getElementById("img1").style
+// 	}
+
+// }
+
+// setInterval(checkTime,500);
